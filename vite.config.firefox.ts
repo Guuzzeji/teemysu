11c4bb51a2d060da9bf793560d2d3d24a6a0dd16
Guuzzeji <5lh8dl440f@gmail.com>
2026-07-11 01:16:43 -0700

    feat(sidePanel): Enable side panel feature for both chrome and firefox

--- vite.config.firefox.ts
+++ vite.config.firefox.ts
@@ -1,9 +1,9 @@
-import { resolve } from 'path';
-import { mergeConfig, defineConfig } from 'vite';
-import { crx, ManifestV3Export } from '@crxjs/vite-plugin';
-import baseConfig, { baseManifest, baseBuildOptions } from './vite.config.base'
+import { resolve } from "path";
+import { mergeConfig, defineConfig } from "vite";
+import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
+import baseConfig, { baseManifest, baseBuildOptions } from "./vite.config.base";
 
-const outDir = resolve(__dirname, 'dist_firefox');
+const outDir = resolve(__dirname, "dist_firefox");
 
 export default mergeConfig(
   baseConfig,
@@ -12,20 +12,23 @@ export default mergeConfig(
       crx({
         manifest: {
           ...baseManifest,
-          background: {
-            scripts: [ 'src/pages/background/index.ts' ]
+          sidebar_action: {
+            default_panel: "src/pages/panel/index.html",
           },
-        } as ManifestV3Export,
-        browser: 'firefox',
+          background: {
+            scripts: ["src/pages/background/index.ts"],
+          },
+        } as unknown as ManifestV3Export,
+        browser: "firefox",
         contentScripts: {
           injectCss: true,
-        }
-      })
+        },
+      }),
     ],
     build: {
       ...baseBuildOptions,
-      outDir
+      outDir,
     },
-    publicDir: resolve(__dirname, 'public'),
-  })
-)
+    publicDir: resolve(__dirname, "public"),
+  }),
+);
